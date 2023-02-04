6db1deaee6419a1c00379a5c5871f7a080de42af
Tsyren Dambaev <tdambaev@gmail.com>
2023-02-04 09:11:14 -0600

    update

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 # Create a VPC
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_support = true 
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
 }
 
 resource "aws_subnet" "public_subnet1" {
-  depends_on = [                             # Explicit dependency
-        aws_vpc.example                        
+  depends_on = [ # Explicit dependency
+    aws_vpc.example
 
   ]
 
-  vpc_id     = aws_vpc.example.id            # Implicit dependency
+  vpc_id     = aws_vpc.example.id # Implicit dependency
   cidr_block = "10.0.1.0/24"
 
 }
--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 # resource "aws_vpc"
 # "example"
 
-output owner_id {
-    value =aws_vpc.example.owner_id 
+output "owner_id" {
+  value = aws_vpc.example.owner_id
 
 }
-output default_security_group_id {
-    value =aws_vpc.example.default_security_group_id
+output "default_security_group_id" {
+  value = aws_vpc.example.default_security_group_id
 
 }
 
-output id {
-    value =aws_vpc.example.id
+output "id" {
+  value = aws_vpc.example.id
 
 }
